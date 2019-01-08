d7bdded2b53c31a3e382445c12b22ae44a07e3ee
Dangield <Dan.gield@gmail.com>
2019-01-08 18:37:41 +0100

    5 sekund przerwy po każdym uczeniu

--- modelowanie.m
+++ modelowanie.m
@@ -1,3 +1,4 @@
+clear variables
 load danewer
 load daneucz
 osiagi = fopen('osiagi.txt','w');
@@ -18,11 +19,13 @@ for i = 1:10
         fprintf(ustawienia,'%d %d %d %d %d %f %d %d',tau,nb,na,i,maxEpoch,err,algucz,trybucz);
         fclose(ustawienia);
         system('sieci.exe');
+        pause(5);
         model
         y_mod = siec(x_wer,y_wer,w10,w20,w1,w2);
-        Ewer = (y_wer(S:end)-y_mod(S:end))'*(y_wer(S:end)-y_mod(S:end));
+        Ewer = (y_wer(S:end)-y_mod(S:end))'*(y_wer(S:end)-y_mod(S:end))
         y_mod = siec(x_ucz,y_ucz,w10,w20,w1,w2);
-        Eucz = (y_ucz(S:end)-y_mod(S:end))'*(y_ucz(S:end)-y_mod(S:end));
+        Eucz = (y_ucz(S:end)-y_mod(S:end))'*(y_ucz(S:end)-y_mod(S:end))
+%         uczenie
         if (Ewer < bestEwer(i))
             bestEwer(i)=Ewer;
             bestEucz(i)=Eucz;
