a99d8173e3efec5bb30275ce28c7eef72fa49a5a
Dangield <Dan.gield@gmail.com>
2019-01-11 12:34:58 +0100

    zmiana nazwy zmiennej na niekolidującą z istniejącą funkcją

--- funregno.m
+++ funregno.m
@@ -1,4 +1,4 @@
-function fcelu = funregno(x)
+function fc = funregno(x)
     global N Nu lambda w1 w10 w2 w20 yz y u du ddmc k;
     
     for p = 0:Nu-1
@@ -11,17 +11,17 @@ function fcelu = funregno(x)
         du(k+p) = u(k+p) - u(k+p-1);
     end
     
-    fcelu = 0;
+    fc = 0;
     for p = 1:N
         wesn = [u(k-3+p) u(k-4+p) y(k-1+p) y(k-2+p)]';
         y(k+p) = w20 + w2*tanh(w10+w1*wesn) + ddmc;
     end
     
     for p=1:N
-        fcelu = fcelu + (yz(k)-y(k+p))^2;
+        fc = fc + (yz(k)-y(k+p))^2;
     end
     for p = 0:Nu-1
-        fcelu = fcelu + lambda*du(k+p)^2;
+        fc = fc + lambda*du(k+p)^2;
     end
     
 end
