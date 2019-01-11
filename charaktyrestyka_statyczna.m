6ecf905b776be3445dd33d66a9ba2ced37108f1d
Dangield <Dan.gield@gmail.com>
2019-01-11 15:44:38 +0100

    sprawko: symulowanie procesu

--- charaktyrestyka_statyczna.m
+++ charaktyrestyka_statyczna.m
@@ -8,4 +8,8 @@ beta_2 = 0.013521;
 for i=1:n
     y(i)=g_2(((beta_1+beta_2)*g_1(u(i)))/(1+alpha_1+alpha_2));
 end
+figure
 plot(u,y)
+title('Charakterystyka statyczna y(u)')
+xlabel('Sterowanie u')
+ylabel('Wyjœcie y')
